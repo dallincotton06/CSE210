7a1c103701ebcfda471b1a71933b29b3efe2a71e
dallincotton06 <dallincotton@gmail.com>
2024-10-31 20:09:03 -0600

    Completed Prep4 and Prep5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -2,8 +2,19 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    static void Main(string[] args) {
+        List<int> numbers = new List<int>();
+        int numberToAdd;
+        do {
+            Console.WriteLine("Enter a Number (Enter 0 when finished): ");
+            numberToAdd = int.Parse(Console.ReadLine());
+            if (numberToAdd != 0) numbers.Add(numberToAdd);
+        } while (numberToAdd != 0);
+
+        Console.WriteLine("The Sum of the list is: " + numbers.Sum());
+        Console.WriteLine("The Average of the list is: " + numbers.Average());
+        Console.WriteLine("The Min of the list is: " + numbers.Min());
+        Console.WriteLine("The Max of the list is: " + numbers.Max());
+
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,30 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep5 World!");
+    static void Main(string[] args) {
+        displayWelcome();
+        displayResult(PromptUserName(), squareNumber(PromptUserNumber()));
+    }
+
+    static void displayWelcome() {
+        Console.WriteLine("Welcome to the Program! ");
+    }
+
+    static String PromptUserName() {
+        Console.WriteLine("What is your Name? ");
+        return Console.ReadLine();
+    }
+
+    static int PromptUserNumber() {
+        Console.WriteLine("What is your Favority Number? ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    static int squareNumber(int number) {
+        return number * number;
+    }
+
+    static void displayResult(String name, int squaredNumber) {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
     }
 }
